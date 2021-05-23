9f3b0bf0461209a90f1e449f0461e27d9d6724be
levanduy <levanduyavatar@gmail.com>
2021-05-23 14:34:52 +0700

    add freature retrieving data from firebase

--- lib/app_screens/traffic_signs_list.dart
+++ lib/app_screens/traffic_signs_list.dart
@@ -2,6 +2,7 @@ import 'package:classify_traffic_sign/app_screens/traffic_sign_details.dart';
 import 'package:classify_traffic_sign/model/traffic_signs.dart';
 import 'package:flutter/material.dart';
 import 'package:classify_traffic_sign/widget/search_widget.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class TrafficSignsList extends StatefulWidget {
   @override
@@ -15,7 +16,26 @@ class TrafficSignsListState extends State<TrafficSignsList>{
   @override
   void initState() {
     super.initState();
-
+    //get list from fire base
+    DatabaseReference databaseReference =  FirebaseDatabase.instance
+        .reference().child("Data");
+    databaseReference.once().then((DataSnapshot dataSnapshot){
+      trafficSignsList.clear();
+      print(dataSnapshot.value);
+      var keys = dataSnapshot.value.keys;
+      var values = dataSnapshot.value;
+      for(var key in keys){
+        TrafficSigns trafficSigns = new TrafficSigns(
+          values [key]['title'],
+          values [key]['description'],
+          values [key]['imageUrl'],
+        );
+        trafficSignsList.add(trafficSigns);
+      }
+      setState(() {
+        print('Length : ${trafficSignsList.length}');
+      });
+    });
     trafficSigns = trafficSignsList;
   }
 
